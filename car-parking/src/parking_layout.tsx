2604021988c4a62e862171524a276f43968a5a05
SrinidhiSasi <srinidhisasidharan@gmail.com>
2023-04-19 14:39:10 -0400

    parkingslot working

--- car-parking/src/parking_layout.tsx
+++ car-parking/src/parking_layout.tsx
@@ -1,21 +1,47 @@
 import './App.css'
-
+import carT from './lib/carE.png'
+import carF from './lib/parking.png'
 export default function ParkingL(){
     let count = 4;
+    
     return(
         // <div className = "box">
-            <div className='parking1'>Parking Spot 1</div>
+            <div className='parking1'>
+            <center>
+            {ParkingIconGenerator()}
+            </center>
+            </div>
         // </div>
         
-        
-    )
+    );
 }
 
-function parkingIconGenerator(){
+
+function ParkingIconGenerator(){
+    let status=[true,true,true,false]
+    let a:any =Array.from({length:4},(v,i)=>i);
+    console.log("lnegth",a);
     
-    return(
+   let ret=a.map((i:number,id:number) => {
+            console.log("here");
+            return <img id={`${id}`} onClick={book} src={status[id]?carT:carF} key={id} className='car' ></img>
+        });
+        
+        console.log(ret);
+        function book(event:any){
+            let el=event.target;
+            let elId=el.getAttribute("id");
+            console.log(el.src.split("3000")[1])
+            if (status[elId])alert(" occupied")
+            else {
+                console.log(carT,carF)
+                console.log(el.src===carF)
+                if(el.src.split("3000")[1]===carT)el.src=carF
+                else el.src=carT
+                alert(el.src)
+            }
+        }
+        return ret;
     
-           <div>hi</div> 
     
-    );
 }
