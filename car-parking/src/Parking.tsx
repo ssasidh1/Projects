1096cda0f3b52626ae6d4e05215e5d2981008a24
akash kumar <akashkumar@akashs-MacBook-Air.local>
2023-05-01 11:29:54 -0400

    Safe stage II

--- car-parking/src/Parking.tsx
+++ car-parking/src/Parking.tsx
@@ -7,7 +7,7 @@ export default function ParkingF(props:any){
     return(
         // <div className = "box">
             <div className='parking_full'>
-             <h3 className={'lotId'}> {props.lotId}</h3>
+             <h3 className={'lotId'}> {"Lot "+props.lotId}</h3>
                 <ParkingL state={props.state} />
                 <ParkingS />
             </div>
