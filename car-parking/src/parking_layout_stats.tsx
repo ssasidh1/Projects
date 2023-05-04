39a3d4f6769e82d0c99a300badda82fd6cdd10f8
ssasidh1 <125688278+ssasidh1@users.noreply.github.com>
2023-05-04 17:24:58 -0400

    commented the sns part

--- car-parking/src/parking_layout_stats.tsx
+++ car-parking/src/parking_layout_stats.tsx
@@ -1,35 +1,35 @@
 import './App.css'
 import { Amplify, Auth } from 'aws-amplify';
 export default function ParkingS(){
- function getUser():any{
-    Auth.currentAuthenticatedUser().then((user)=>{
-        const email=(user.attributes.email);console.log(email);
-        return fetch('https://fe8us6zfr1.execute-api.us-west-1.amazonaws.com/stage1',{
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': 'http://localhost:3000',
+ //function getUser():any{
+//     Auth.currentAuthenticatedUser().then((user)=>{
+//         const email=(user.attributes.email);console.log(email);
+//         return fetch('https://fe8us6zfr1.execute-api.us-west-1.amazonaws.com/stage1',{
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Access-Control-Allow-Origin': 'http://localhost:3000',
             
-              },
-            body: JSON.stringify({email})
-        });
-    }).then(res =>{
-            if(res.ok){
-                return res.json();
-            }
-            else{
-                throw new Error('Network response was not ok.');
-            }
-          })
-          .then(data => {
-            console.log(data);
-          })
-          .catch(error => {
-            console.error('There was an error!', error);
-          });
+//               },
+//             body: JSON.stringify({email})
+//         });
+//     }).then(res =>{
+//             if(res.ok){
+//                 return res.json();
+//             }
+//             else{
+//                 throw new Error('Network response was not ok.');
+//             }
+//           })
+//           .then(data => {
+//             console.log(data);
+//           })
+//           .catch(error => {
+//             console.error('There was an error!', error);
+//           });
 
    
-}
+//}
     return(
         // <div className = "box">
             <div className='stats'>
